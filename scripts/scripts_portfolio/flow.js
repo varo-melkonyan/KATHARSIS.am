aadd9ca91b7c2403f71a260c530f7a243bda8a85
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2022-09-07 23:46:12 +0400

    ..

--- scripts/scripts_portfolio/flow.js
+++ scripts/scripts_portfolio/flow.js
@@ -14,10 +14,22 @@ async function onLoad() {
     const onClick = (event) => {
       viewPfItem.pgf = set.data[event.path[1].id].img_brand_items;
       console.log(viewPfItem.pgf);
+      set_image_path();
     }
 
     for (let j = 0; j < document.getElementsByClassName("img-pf").length; j++) {
       const element = document.getElementsByClassName("img-pf")[j];
       element.addEventListener('click', onClick);
     }
+
+    function set_image_path(){
+      for (let k = 0; k < viewPfItem.pgf.length; k++) {
+        var image_path = `${viewPfItem.pgf[k]}`
+        let pfItem = `
+          <img src="${image_path}">
+        `;
+        $("#content_pf_item").append(pfItem);
+      }
+    }
+    
 }
