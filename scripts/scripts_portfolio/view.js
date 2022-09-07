58828b95942ebf7bfb6f803ed39a3712ffccf9e2
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2022-09-07 16:12:26 +0400

    ..

--- scripts/scripts_portfolio/view.js
+++ scripts/scripts_portfolio/view.js
@@ -1,24 +1,30 @@
 const view = {
     createPortfolio: (i, title, type, img_brand) => {
         let portfolio = `
-            <div id="${i}" class="card ${i}">
-                <img src="${img_brand}" class="img-pf"></img>
-                <div class="pf_text_holder pf_text_holder_${i}">
-                    <h1 class="title">${title}</h1>
-                    <h6 class="type">${type}</h6>
+
+                <div id="${i}" class="card ${i}">
+                    <img src="${img_brand}" class="img-pf"></img>
+                    <div class="pf_text_holder pf_text_holder_${i}">
+                        <h1 class="title">${title}</h1>
+                        <h6 class="type">${type}</h6>
+                    </div>
                 </div>
-            </div>
         `;
 
         $("#content_pf").append(portfolio);
         $(`#${i}`).css("margin-left", "1.2vw");
-
+        
         $(`#${i}`).hover(function() {
             $(`.pf_text_holder_${i}`).css("height","auto");
             }, function() {
                 $(`.pf_text_holder_${i}`).css("height","0");
         }); 
+
+        
     }
 };
-
+const onClick = (event) => {
+    console.log(event.path[1].id);
+  }
+  window.addEventListener('click', onClick);
 onLoad();
