49fd924e45f36636d219ccc5e2b88afd43f7a78e
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2022-09-07 12:17:38 +0400

    `

--- scripts/scripts_portfolio/view.js
+++ scripts/scripts_portfolio/view.js
@@ -3,12 +3,15 @@ const view = {
         let portfolio = `
             <div id="${i}" class="card ${i}">
                 <img src="${img_brand}" class="img-pf"></img>
-                <p class="title">${title}</p>
-                <p class="type">${type}</p>
+                <div class="pf_text_holder">
+                    <h1 class="title">${title}</h1>
+                    <h6 class="type">${type}</h6>
+                </div>
             </div>
         `;
 
         $("#content_pf").append(portfolio);
+        $(`#${0}`).css("margin-left", 0);
         $(`#${i}`).css("margin-left", 24);
     }
 };
