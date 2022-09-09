e9284d60db29e9e78e6c65446c16d88f2e0197c5
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2022-09-09 12:39:17 +0400

    Update view.js

--- scripts/scripts_portfolio/view.js
+++ scripts/scripts_portfolio/view.js
@@ -2,7 +2,7 @@ const view = {
     createPortfolio: (i, title, type, img_brand) => {
         let portfolio = `
                 <div id="${i}" class="card ${i}">
-                    <img src="${img_brand}" class="img-pf" href="portfolio_items.html"></img>
+                    <img src="${img_brand}" class="img-pf"></img>
                     <div class="pf_text_holder pf_text_holder_${i}">
                         <h1 class="title">${title}</h1>
                         <h6 class="type">${type}</h6>
