808c209fec59a969c3584f9f3c668ba17c527086
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2022-09-09 10:11:32 +0400

    ..

--- scripts/scripts_portfolio/flow.js
+++ scripts/scripts_portfolio/flow.js
@@ -1,35 +1,38 @@
 let set = { index: 0 };
 
 async function onLoad() {
-    let path = "../../data/data-portfolio/EN/en-data.json"
-    await $.get(path, function (json) { 
-      set.data = json.data;
-    });
+  if (true) {
 
-    for (let i = 0; i < set.data.length; i++) {
-      view.createPortfolio(i, set.data[i].title, set.data[i].type, set.data[i].img_brand)
-    }
-    
-    const viewPfItem = {};
-    const onClick = (event) => {
-      viewPfItem.pgf = set.data[event.path[1].id].img_brand_items;
-      console.log(viewPfItem.pgf);
-      set_image_path();
-    }
+  }
+  let path = "../../data/data-portfolio/EN/en-data.json"
+  await $.get(path, function (json) { 
+    set.data = json.data;
+  });
 
-    for (let j = 0; j < document.getElementsByClassName("img-pf").length; j++) {
-      const element = document.getElementsByClassName("img-pf")[j];
-      element.addEventListener('click', onClick);
-    }
+  for (let i = 0; i < set.data.length; i++) {
+    view.createPortfolio(i, set.data[i].title, set.data[i].type, set.data[i].img_brand)
+  }
+  
+  const viewPfItem = {};
+  const onClick = (event) => {
+    viewPfItem.pgf = set.data[event.path[1].id].img_brand_items;
+    console.log(viewPfItem.pgf);
+    set_image_path();
+  }
 
-    function set_image_path(){
-      for (let k = 0; k < viewPfItem.pgf.length; k++) {
-        var image_path = `${viewPfItem.pgf[k]}`
-        let pfItem = `
-          <img src="${image_path}">
-        `;
-        $("#content_pf").append(pfItem);
-      }
+  for (let j = 0; j < document.getElementsByClassName("img-pf").length; j++) {
+    const element = document.getElementsByClassName("img-pf")[j];
+    element.addEventListener('click', onClick);
+  }
+
+  function set_image_path(){
+    for (let k = 0; k < viewPfItem.pgf.length; k++) {
+      var image_path = `${viewPfItem.pgf[k]}`
+      let pfItem = `
+        <img src="${image_path}">
+      `;
+      $("#content_pf").append(pfItem);
     }
-    
+  }
+  
 }
