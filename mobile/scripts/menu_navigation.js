ff1f210958e4d06e6f563d30cd51876078d91442
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2024-03-23 15:01:56 +0400

    ee

--- mobile/scripts/menu_navigation.js
+++ mobile/scripts/menu_navigation.js
@@ -1,13 +1,13 @@
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollpos = window.pageYOffset;
-    if(prevScrollpos > currentScrollpos) {
-        document.getElementById("header").style.top = "0";
-    } else {
-        document.getElementById("header").style.top = "-100px";
-    }
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollpos = window.pageYOffset;
+//     if(prevScrollpos > currentScrollpos) {
+//         document.getElementById("header").style.top = "0";
+//     } else {
+//         document.getElementById("header").style.top = "-100px";
+//     }
 
-    prevScrollpos = currentScrollpos;
+//     prevScrollpos = currentScrollpos;
 
-}
+// }
